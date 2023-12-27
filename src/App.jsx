eaa76e985ae70494627cd280fd6301c99765ef95
Mariano Masondo <nanomason@gmail.com>
2023-12-27 16:16:58 -0300

    gitIgnore

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Container, Box, Typography } from "@mui/material";
 import Navbar from "./components/navBar";
 
 const API_WEATHER = `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${import.meta.env.VITE_API_KEY}&q=`;
+console.log("URL de la solicitud:", API_WEATHER);
 
 export default function App() {
   const [city, setCity] = useState("");
