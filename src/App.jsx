08c0b7e55a9911428b7e2eb9bfe4cf5223557aed
Mariano Masondo <nanomason@gmail.com>
2026-05-01 14:19:13 -0300

    EsLink fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { useState, useEffect } from "react";
 import { Container, Box, Typography } from "@mui/material";
 import Navbar from "./components/navBar";
