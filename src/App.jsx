6869bff8395dc8a28592fd1c29a251e067ec9cf9
Mariano Masondo <nanomason@gmail.com>
2026-05-14 16:55:50 -0300

    Lang

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import "./App.css";
 const API_WEATHER =
   // "https://api.worldweatheronline.com/premium/v1/weather.ashx?key=fe3597c6c0f04584b1e173127230512&q=";
   
-  "ffdd5d05d8f6478398e195037261405";
+  "https://api.weatherapi.com/v1/forecast.json?key=ffdd5d05d8f6478398e195037261405&lang=es";
 
 const DEFAULT_CITY = "Cordoba, Argentina";
 
